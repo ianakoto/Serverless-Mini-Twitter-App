4f5150b89eaee889a89b87d20ee48ac7d01a5670
ian akoto <iancecilakoto@gmail.com>
2020-06-11 18:34:59 +0000

    client setup with auth0 configurations

---
+++ client/MiniTwitter/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {}
+
+}
